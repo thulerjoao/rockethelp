ba111115dd980420648a5d6d1b6d6c854d6a79d5
thulerjoao <thuler_lima@hotmail.com>
2023-03-04 22:27:33 -0300

    feat: created singIn page and stylized statusbar

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
-import { NativeBaseProvider } from "native-base";
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import React from 'react';
 import SignIn from './src/screens/SingIn';
+import { THEME } from "./src/styles/theme";
+import Loading from "./src/components/loading";
 
 
 export default function App() {
+  const[fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
   return (
-    <NativeBaseProvider>
-      <SignIn/>
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+        />
+      {fontsLoaded? <SignIn/>: <Loading/>}
+      
     </NativeBaseProvider>
   );
 }
